a3d7829b80bcb871a658facb8c6f4fb3b87bc0b8
fengmushu <fengmushu@gmail.com>
2019-04-23 10:32:29 +0800

    finished startup.sh

--- hotfix/startup.sh
+++ hotfix/startup.sh
@@ -1,3 +1,39 @@
 #!/bin/sh
 
-echo xxxxxxx > /tmp/xxx.log
+PWD=`pwd`
+
+OSGI=/usr/osgi/
+FRAMEWORK=$OSGI/felix-framework
+BUNDLE=$FRAMEWORK/bundle
+CONF=$FRAMEWORK/conf/config.properties
+CONF_backup="${CONF}.backup"
+PLUGIN=com.chinatelecom.smartgateway.hproxy.jar
+
+
+stop_jamvm()
+{
+    killall cfg_manager
+    killall jamvm
+}
+
+start_jamvm()
+{
+    cd $FRAMEWORK
+    rm /tmp/felix-cache -fr
+    ../bin/jamvm -Dfile.encoding=UTF-8 -Xms64M -Xss1M -Xmx128M -jar bin/felix.jar
+}
+
+
+##### setup files
+# cp $PLUGIN $BUNDLE/
+ln -sf $PWD/$PLUGIN $BUNDLE/$PLUGIN 
+
+##### update config
+[ -f $CONF_backup ] || cp $CONF $CONF_backup
+sed -r 's#mangement\.jar#mangement.jar \\\r\n file:bundle\/com.chinatelecom.smartgateway.hproxy.jar#g' <  $CONF_backup > $CONF
+
+##### restart jamvm
+stop_jamvm
+start_jamvm
+
+
