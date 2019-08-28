fe4a2ba73698b8200a19d06a389eb9e4342a5e4e
fengmushu <fengmushu@gmail.com>
2019-08-28 19:50:48 +0800

    fixup halt script blocked

--- hotfix-h3/startup.sh
+++ hotfix-h3/startup.sh
@@ -3,14 +3,20 @@
 # set -x
 
 PWD=`pwd`
+KVER=`uname  -r`
 
 copy_files() {
+    cd $PWD
     sudo cp -a ./fakeroot/* /
 }
 
 do_hotfix() {
     # fixup timezone
     sudo ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+    # fixup modules dir
+    cd /lib/modules && {
+        [ -d $KVER ] || [ -L $KVER ] || sudo ln -s 3.4.113-sun8i $KVER
+    }
 }
 
 main()
